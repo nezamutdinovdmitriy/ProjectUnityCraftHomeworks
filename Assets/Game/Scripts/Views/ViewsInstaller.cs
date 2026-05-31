37c95fcb9f52f6f4ca14141933b476d91ae3efe7
nezamutdinovdmitriy <nezamutdinovdmitriy@gmail.com>
2026-06-01 01:19:21 +0500

    Update ViewsInstaller.cs

--- Assets/Game/Scripts/Views/ViewsInstaller.cs
+++ Assets/Game/Scripts/Views/ViewsInstaller.cs
@@ -1,11 +1,20 @@
+using UnityEngine;
 using Zenject;
 
 namespace Game.Views
 {
     public sealed class ViewsInstaller : MonoInstaller
     {
+        [SerializeField]
+        private PlanetPopupView _planetPopupView;
+
+        [SerializeField]
+        private PlanetView _planetView;
+        
         public override void InstallBindings()
         {
+            Container.Bind<PlanetPopupView>().FromInstance(_planetPopupView).AsSingle();
+            Container.Bind<PlanetView>().FromInstance(_planetView).AsSingle();
             //TODO:
         }
     }
