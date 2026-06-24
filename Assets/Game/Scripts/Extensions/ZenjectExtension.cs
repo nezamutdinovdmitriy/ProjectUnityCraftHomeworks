5215423737f86f7ee5df00916a2166ec698abe7f
nezamutdinovdmitriy <nezamutdinovdmitriy@gmail.com>
2026-06-24 23:11:14 +0500

    Update ZenjectExtension.cs

--- Assets/Game/Scripts/Extensions/ZenjectExtension.cs
+++ Assets/Game/Scripts/Extensions/ZenjectExtension.cs
@@ -1,12 +1,21 @@
-﻿using Zenject;
+﻿using System.Runtime.CompilerServices;
+using Zenject;
 
 namespace Game.Scripts.Extensions
 {
     public static class ZenjectExtension
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Install(DiContainer container, Installer installer)
         {
             container.Inject(installer);
+            installer.InstallBindings();
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Install(DiContainer container, Installer installer, params object[] extraArgs)
+        {
+            container.Inject(installer, extraArgs);
             installer.InstallBindings();
         }
     }
