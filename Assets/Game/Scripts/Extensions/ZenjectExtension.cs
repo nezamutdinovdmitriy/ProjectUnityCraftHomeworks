7531d74797d78f39def1e4c538ab6a4d91278100
nezamutdinovdmitriy <nezamutdinovdmitriy@gmail.com>
2026-06-24 23:08:46 +0500

    zenject imported

---
+++ Assets/Game/Scripts/Extensions/ZenjectExtension.cs
@@ -0,0 +1,13 @@
+﻿using Zenject;
+
+namespace Game.Scripts.Extensions
+{
+    public static class ZenjectExtension
+    {
+        public static void Install(DiContainer container, Installer installer)
+        {
+            container.Inject(installer);
+            installer.InstallBindings();
+        }
+    }
+}
